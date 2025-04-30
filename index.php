49c481c6ddae7fd41070026023c5f8f6f8371bad
praboth-mbp <probothcharith@gmail.com>
2025-05-01 00:07:20 +0530

    Update index.php to change title and integrate Tailwind CSS for improved styling

--- index.php
+++ index.php
@@ -4,17 +4,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Final Project</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Next Gen Parts</title>
 </head>
 
 <body>
-
-    <input type="text" id="email" placeholder="Email">
-    <input type="password" id="password" placeholder="Password">
-
-    <button id="register">Register</button>
-
-    <a href="http://localhost/nextgen-parts/login.php">Login</a>
 
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
